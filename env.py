16e2bf7d45c5f00fcbb09f2db875669780835cec
Yuri Tsyganenko <yuri.tsyganenko@moonpig.com>
2022-08-07 16:07:08 +0400

    Initial version - grab all links and list them

---
+++ env.py
@@ -0,0 +1,33 @@
+import pytest
+import requests
+# import json
+
+
+class Env:
+    def __init__(self, name: str, host_url: str):
+        self.name = name
+        self.host_url = host_url
+        self.session = None
+
+
+    def get(self, url):
+        return requests.get(url)
+
+    def get_endpoint(self, endpoint: str):
+        """
+        endpoint is part of URL to be applied with host
+        return requests.Response
+        """
+        return requests.get(f'{self.host_url}{endpoint}')   #  "data": json.loads(res.text)
+
+
+def env_():
+    """ To be used in fixture as well as outside"""
+    return Env("w3", "https://www.w3.org/")
+
+
+@pytest.fixture()
+def env():
+    return env_()
+
+
--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,4 @@
-from env import env_
+from env import Env, env, env_
 
 
 def grab_all_links(htm: str):
@@ -8,13 +8,48 @@ def grab_all_links(htm: str):
     # print(soup.prettify())
     # print(" ************** END-OF-Prettify *******************  ")
 
-    links = [link.get('href') for link in soup.find_all('a')]
+    links = [link.get('href') for link in soup.find_all('a')
+             if link not in ['/', ""]   # skip trash ones
+             ]
     print(f"==>> Found {len(links)} links")
     return links
 
 
+def link_to_full_url(env: Env, link: str):
+    """ Convert link to full format:
+    Not changing those already in the full format
+    Prevents doubling slash - from host ending and link start symbol
+    """
+    if link.startswith("https://"):
+        return link
+    else:
+        # remove extra slash: when host_url ends with '/' and the link itself
+        if env.host_url[-1:] == '/' and link[0] == '/':
+            return env.host_url[:-1] + link
+        else:
+            return env.host_url + link
+
+
+def pytest_generate_tests(metafunc):
+    print("** pytest_generate_tests ** ")
+    # print(metafunc.fixturenames)
+
+    if "link" in metafunc.fixturenames:
+        htm = env_().get_endpoint('').text
+        links = [link_to_full_url(env=env_(), link=link) for link in grab_all_links(htm)]
+        print(links)
+        metafunc.parametrize("link", links, ids=links)
+
+
+def test_link_valid(env, link: str):
+    print(f" >> testing link {link} --{link[-1]}")
+    res = env.get(link)
+    assert res.status_code == 200
+
+
 if __name__ == "__main__":
-    htm = env_().get('/').text
+    htm = env_().get_endpoint('/').text
     for link in grab_all_links(htm):
-        print(link)
+        # print(link)
+        print(link_to_full_url(env=env_(), link=link))
 
